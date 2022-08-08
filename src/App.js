df9d30ec7c27822f4b7780b988059e9fce49145c
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-08 19:51:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [val, set] = useState("");
+  const [phrase, setPhrase] = useState("example phrase");
+
+  const createPhrase = () => {
+    setPhrase(val);
+    set("");
+  };
+
+  useEffect(() => {
+    console.log(`typing "${val}"`);
+  });
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`);
+  });
+
+  return (
+    <>
+      <label>Favorite phrase:</label>
+      <input
+        value={val}
+        placeholder={phrase}
+        onChange={(e) => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
+    </>
+  );
+};
+
+export default App;
