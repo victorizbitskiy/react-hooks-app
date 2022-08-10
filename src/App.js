0d1fc1b94587cd8deeb5d081930236aeb06d7dbc
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-08-10 21:47:52 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 import Cat from "./components/Cat";
 import { memo } from "react";
+import { useCallback } from "react";
 
-const PureCat = memo(Cat, () => true);
+const PureCat = memo(Cat);
 
 const App = () => {
   const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
+  const meow = useCallback((name) => console.log(`${name} has meowed`), []);
   return (
     <>
       {cats.map((name, i) => (
-        <PureCat key={i} name={name} meow={name => console.log(`${name} has meowed`)}/>
+        <PureCat key={i} name={name} meow={meow}/>
       ))}
       <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
         Add a Cat
